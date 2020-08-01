81f7fdf77f3055303e455d6d602702bdfc28b33f
saiumesh535 <d.sai535@gmail.com>
2020-08-01 11:27:19 +0530

    added database example

--- database/main.go
+++ database/main.go
@@ -1,9 +1,95 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/go-slide/slide"
+	"github.com/jmoiron/sqlx"
+	"github.com/jmoiron/sqlx/types"
+	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
+type Input struct {
+	Username string `json:"username" db:"username"`
+	Password string `json:"password" db:"password"`
+	Age      int    `json:"age" db:"age"`
+}
+
+type User struct {
+	Email string         `json:"email" db:"email"`
+	Input types.JSONText `json:"input" db:"input"`
+	Tags  pq.Int64Array `json:"tags" db:"tags"`
+}
+
 func main() {
-	fmt.Println("hey!")
+	db, err := sqlx.Connect("postgres", "postgres://postgres:postgres@localhost:5432/rust_postgres_server?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	app := slide.InitServer(&slide.Config{})
+	app.Get("/users", func(ctx *slide.Ctx) error {
+		users := new([]User)
+		if err := db.Select(users, "SELECT email, input, tags from json_table"); err != nil {
+			return ctx.Send(http.StatusInternalServerError, err.Error())
+		}
+		return ctx.JSON(http.StatusOK, map[string]interface{}{
+			"payload": users,
+		})
+	})
+	app.Get("/adduser", func(ctx *slide.Ctx) error {
+		user := new(User)
+		user.Email = "someemail@gmail.com"
+		input := Input{
+			Username: "Slide",
+			Age:      1,
+			Password: "somePassword",
+		}
+		inputJson, _ := json.Marshal(&input)
+		user.Input = inputJson
+		_, err := db.NamedExec("INSERT INTO json_table (email, input) VALUES (:email, :input)", user)
+		if err != nil {
+			return ctx.Send(http.StatusInternalServerError, err.Error())
+		}
+		return ctx.SendStatusCode(http.StatusOK)
+	})
+	app.Get("/filter/:age", func(ctx *slide.Ctx) error {
+		users := new([]User)
+		if err := db.Select(users, "SELECT email, input from json_table"); err != nil {
+			return ctx.Send(http.StatusInternalServerError, err.Error())
+		}
+		age, _ := strconv.Atoi(ctx.GetParam("age"))
+		filteredUsers := new([]string)
+		for _, v := range *users {
+			input := new(Input)
+			err := json.Unmarshal(v.Input, input)
+			if err != nil {
+				return ctx.Send(http.StatusInternalServerError, err.Error())
+			}
+			if input.Age == age {
+				*filteredUsers = append(*filteredUsers, input.Username)
+			}
+		}
+		return ctx.JSON(http.StatusOK, filteredUsers)
+	})
+	app.Get("/filter/tags/:number", func(ctx *slide.Ctx) error {
+		users := new([]User)
+		if err := db.Select(users, "SELECT email, input, tags from json_table"); err != nil {
+			return ctx.Send(http.StatusInternalServerError, err.Error())
+		}
+		tagNumber, _ := strconv.ParseInt(ctx.GetParam("number"), 10, 64)
+		filteredUsers := new([]string)
+		for _, v := range *users {
+			for _, t := range v.Tags {
+				if t == tagNumber {
+					*filteredUsers = append(*filteredUsers, v.Email)
+				}
+			}
+		}
+		return ctx.JSON(http.StatusOK, filteredUsers)
+	})
+	log.Fatal(app.Listen("localhost:3000"))
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module slide_examples
 go 1.14
 
 require (
-	github.com/go-slide/slide v1.1.8 // indirect
-	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/go-slide/slide v1.1.8
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/klauspost/compress v1.10.10 // indirect
+	github.com/lib/pq v1.8.0
 	github.com/valyala/fasthttp v1.15.1 // indirect
+	honnef.co/go/tools v0.0.1-2020.1.4 // indirect
 )
